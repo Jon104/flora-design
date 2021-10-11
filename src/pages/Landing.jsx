e2ce134fbcaa44c942d2c255fb0d9fbb5d9198f8
Jonathan Girard <jonathan.girard@vooban.com>
2021-10-11 08:25:57 -0400

    Added middle section to landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -32,6 +32,44 @@ const Title = styled.p`
   text-shadow:1px 1px 5px #9F2E0E;
 `;
 
+const MiddleSection = styled.div`
+  position: relative;
+  display: block;
+  height: 100vh;
+  padding-top: 25%;
+  z-index: 5;
+
+  @media (max-width: 1300px) {
+    padding-top: 20%;
+  }
+`;
+
+const LeftPanel = styled.div`
+  display: inline-block;
+  padding-left: 5rem;
+  padding-right: 5rem;
+  
+  z-index: 20;
+  width: 45%;
+`;
+
+const RightPanel = styled.img`
+  position: absolute;
+  display: inline-block;
+  width: 50%;
+`;
+
+const MiddleSectionTitle = styled.p`
+  font-size: 36px;
+  color: #9F2E0E;
+`;
+
+const MiddleSectionParagraph = styled.p`
+  font-size: 25px;
+  color: #9F2E0E;
+`;
+
+
 const Landing = () => {
   return (
     <>
@@ -40,6 +78,15 @@ const Landing = () => {
       <Container>
         <Title>ART TEXTILE INSPIRÉ DE LA NATURE</Title>
       </Container>
+      <MiddleSection>
+        <LeftPanel>
+          <MiddleSectionTitle>BIENVENUE DANS MON UNIVERS</MiddleSectionTitle>
+          <MiddleSectionParagraph>Je suis Laurie, maman de 2 garçons. J'habite au coeur des belles montagnes de Stoneham. </MiddleSectionParagraph>
+          <MiddleSectionParagraph>Plutôt rêveuse, toujours pleine d'idées, pis un peu intense quand je m'embarque dans quelque chose.</MiddleSectionParagraph>
+          <MiddleSectionParagraph>J'espère réussir à te toucher par mon art. Tu remarqueras sans doute ma signature végétale bien à moi. Par mes oeuvres et mes cours de macramé,  j'ai pour mission d'aider les femmes à se reconnecter à leur essence !</MiddleSectionParagraph>
+        </LeftPanel>
+        <RightPanel src="./img/landing_middle_view.jpg" alt="Landing middle section" />
+      </MiddleSection>
       
     </>
   )
