43345bf8b67968880529bfde4e9e971ee8d957cc
Jonathan Girard <jonathan.girard@vooban.com>
2021-10-25 21:26:12 -0400

    .

--- src/pages/PersonalPiece.jsx
+++ src/pages/PersonalPiece.jsx
@@ -290,7 +290,8 @@ const PersonalPiece = () => {
           isChecked={isChecked}
           onClick={() => setIsChecked(!isChecked)}
         />
-        <form name="contact" netlify>
+        <form name="contact" method="post" netlify>
+          <input type="hidden" name="form-name" value="contact" />
           <p>
             <label>
               Name <input type="text" name="name" />
