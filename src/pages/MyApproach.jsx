cd502d67186fc6ac7652d2220d04fcfd9ae7d7ab
Jonathan Girard <jonathan.girard@vooban.com>
2021-11-15 22:02:19 -0500

    Refact

--- src/pages/MyApproach.jsx
+++ src/pages/MyApproach.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Logo = styled.img`
   margin-left: auto;
@@ -24,23 +24,23 @@ const Container = styled.div`
   margin: 5% 5%;
   z-index: 2;
   text-align: center;
-  text-shadow: 1px 1px 3px #9F2E0E;
+  text-shadow: 1px 1px 3px #9f2e0e;
 `;
 
 const Title = styled.p`
-  font-size: 36px;
-  color: #9F2E0E;
+  font-size: 32px;
+  color: #9f2e0e;
 `;
 
 const Subtitle = styled.p`
   font-family: Barlow;
-  font-size: 32px;
-  color: #9F2E0E;
+  font-size: 28px;
+  color: #9f2e0e;
 `;
 
 const MiddleSection = styled.div`
   position: relative;
-  display: block; 
+  display: block;
   height: 1000px;
   margin: 5% 5%;
   padding-top: 20%;
@@ -56,7 +56,7 @@ const MiddleSectionImage = styled.img`
 `;
 
 const Bubble = styled.div`
-  border: 1px solid #F8F8FB;
+  border: 1px solid #f8f8fb;
   box-shadow: 10px 10px 30px rgba(208, 207, 220, 0.4);
   border-radius: 40px;
   display: flex;
@@ -66,7 +66,7 @@ const Bubble = styled.div`
   width: 600px;
   right: 20%;
   position: absolute;
-  
+
   top: 75%;
   text-align: center;
 
@@ -124,36 +124,51 @@ const MyApproach = () => {
       <Container>
         <Title>CRÉONS ENSEMBLE, EN ART-CONNEXION</Title>
         <Subtitle>
-          Parce qu'une oeuvre créée à partir de ton essence, est bien plus porteuse de sens!
+          Parce qu'une oeuvre créée à partir de ton essence, est bien plus
+          porteuse de sens!
           <Subtitle>
-            Ici, le processus que je te propose, c’est l’expérience ultime Flora Design! C’est un processus de co-création. C’est une expérience d’Art-connexion. C'est une rencontre intime avec moi. C'est un voyage à l'intérieur de toi.
+            Ici, le processus que je te propose, c’est l’expérience ultime Flora
+            Design! C’est un processus de co-création. C’est une expérience
+            d’Art-connexion. C'est une rencontre intime avec moi. C'est un
+            voyage à l'intérieur de toi.
           </Subtitle>
-        </Subtitle> 
+        </Subtitle>
       </Container>
 
       <MiddleSection>
         <Title>Imagine...</Title>
         <Subtitle>
-          un voyage avec moi dans lequel je t’aide à aller plus loin à l’intérieur de toi. Où on parvient à se connecter à ton essence, et à la faire se déployer ensemble. L'oeuvre que je ferai pour toi dans ce voyage, ce sera donc un portrait de ton intérieur: 
-        </Subtitle> 
+          un voyage avec moi dans lequel je t’aide à aller plus loin à
+          l’intérieur de toi. Où on parvient à se connecter à ton essence, et à
+          la faire se déployer ensemble. L'oeuvre que je ferai pour toi dans ce
+          voyage, ce sera donc un portrait de ton intérieur:
+        </Subtitle>
         <Subtitle>
-          Ça peut être en lien avec tes souvenirs passés, tes ancrages profonds, les transitions que tu vis actuellement, les émotions qui prédominent à l'intérieur de toi, ce que tu as envie d'exprimer, le rappel de ce à quoi tu rêves... On va le découvrir et l'élaborer ensemble !
-          Je te propose de te laisser guider par ma démarche. 
-          Le tout bien sûr dans le respect de ton rythme. 
-        </Subtitle> 
+          Ça peut être en lien avec tes souvenirs passés, tes ancrages profonds,
+          les transitions que tu vis actuellement, les émotions qui prédominent
+          à l'intérieur de toi, ce que tu as envie d'exprimer, le rappel de ce à
+          quoi tu rêves... On va le découvrir et l'élaborer ensemble ! Je te
+          propose de te laisser guider par ma démarche. Le tout bien sûr dans le
+          respect de ton rythme.
+        </Subtitle>
         <Subtitle>
-          Ultimement, je sais que mettre en lumière les fondements de toi-même, ne peut que t'amener plus de liberté et de clarté intérieure. Te permettre de te sentir plus alignée.
+          Ultimement, je sais que mettre en lumière les fondements de toi-même,
+          ne peut que t'amener plus de liberté et de clarté intérieure. Te
+          permettre de te sentir plus alignée.
         </Subtitle>
         <MiddleSectionImage src="./img/middle_section_long_image.jpeg" />
         <Bubble>
           <BubbleTag>Semaine 1</BubbleTag>
           <BubbleTitle>Respect de la nature</BubbleTitle>
-          <BubbleSubtitle>On prend une quinzaine de minutes au téléphone où je réponds à tes questions et on voit ensemble si la démarche pourrait te convenir. Le tout sans engagement.</BubbleSubtitle>
+          <BubbleSubtitle>
+            On prend une quinzaine de minutes au téléphone où je réponds à tes
+            questions et on voit ensemble si la démarche pourrait te convenir.
+            Le tout sans engagement.
+          </BubbleSubtitle>
         </Bubble>
       </MiddleSection>
-      
     </>
-  )
-}
+  );
+};
 
 export default MyApproach;
