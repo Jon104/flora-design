28e049c42e7b357487bc9b7aafc93690ef185dc4
Jonathan Girard <jonathan.girard@vooban.com>
2021-11-22 23:34:29 -0500

    Adding Boutique

---
+++ src/pages/Boutique.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { useCallback } from "react";
+import { useEffect } from "react";
+import { commerce } from "../lib/commerce";
+
+const Boutique = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const fetchCart = useCallback(() => {
+    commerce.cart
+      .retrieve()
+      .then((cart) => {
+        setCart(cart);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the cart", error);
+      });
+  }, []);
+
+  const fetchProducts = useCallback(() => {
+    commerce.products
+      .list()
+      .then((products) => setProducts(products.data))
+      .catch((error) => {
+        console.log("There was an error fetching the products", error);
+      });
+  }, []);
+
+  const handleAddToCart = (productId, quantity) => {
+    commerce.cart
+      .add(productId, quantity)
+      .then((item) => {
+        setCart(item.cart);
+      })
+      .catch((error) => {
+        console.error("There was an error adding the item to the cart", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchCart();
+    fetchProducts();
+  }, [fetchCart, fetchProducts]);
+
+  return (
+    <>
+      {products.map((product) => (
+        <div>
+          <p>{product.id}</p>
+          <img src={product.image.url} alt="yop" width="300" />
+          <button
+            name="Add to cart"
+            onClick={() => handleAddToCart(product.id, 1)}
+          >
+            Quick add
+          </button>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Boutique;
