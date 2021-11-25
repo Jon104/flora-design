cb81c1cdb8df50fec2a3404692a0663bb47b85ae
Jonathan Girard <jonathan.girard@vooban.com>
2021-11-25 00:16:34 -0500

    Fix

--- src/components/forms/PersonalPieceForm.jsx
+++ src/components/forms/PersonalPieceForm.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import { Box, Grid, ImageList, ImageListItem, Slider } from "@mui/material";
 import { useDropzone } from "react-dropzone";
