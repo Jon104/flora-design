6b7bf0ef37402c74e4bc97684b32644692648bde
Jonathan Girard <jonathan.girard@vooban.com>
2021-12-08 21:48:25 -0500

    Moved Cart to App scope

--- src/App.js
+++ src/App.js
@@ -7,9 +7,15 @@ import CreateTogether from "./pages/CreateTogether";
 import MyApproach from "./pages/MyApproach";
 import PersonalPiece from "./pages/PersonalPiece";
 import Thanks from "./pages/Thanks";
+import Checkout from "./pages/Checkout";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { createTheme, ThemeProvider as Mui } from "@mui/material/styles";
+import { useEffect, useState, useCallback } from "react";
+import { commerce } from "./lib/commerce";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Box, IconButton } from "@mui/material";
+import CartDrawer from "components/cart/CartDrawer";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -48,15 +54,137 @@ const themeMui = createTheme({
 });
 
 function App() {
+  const [isOpen, toggleForm] = useState(false);
+  const [cart, setCart] = useState({});
+  const [products, setProducts] = useState([]);
+  const fetchProducts = useCallback(() => {
+    commerce.products
+      .list()
+      .then((products) => setProducts(products.data))
+      .catch((error) => {
+        console.log("There was an error fetching the products", error);
+      });
+  }, []);
+
+  const fetchCart = useCallback(() => {
+    commerce.cart
+      .retrieve()
+      .then((cart) => {
+        setCart(cart);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the cart", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchCart();
+    fetchProducts();
+  }, [fetchCart, fetchProducts]);
+
+  const handleAddToCart = (productId, quantity) => {
+    commerce.cart
+      .add(productId, quantity)
+      .then((item) => {
+        setCart(item.cart);
+      })
+      .catch((error) => {
+        console.error("There was an error adding the item to the cart", error);
+      });
+  };
+
+  const handleUpdateCartQty = (lineItemId, quantity) => {
+    commerce.cart
+      .update(lineItemId, { quantity })
+      .then((resp) => {
+        setCart(resp.cart);
+      })
+      .catch((error) => {
+        console.log("There was an error updating the cart items", error);
+      });
+  };
+
+  const handleRemoveFromCart = (lineItemId) => {
+    commerce.cart
+      .remove(lineItemId)
+      .then((resp) => {
+        setCart(resp.cart);
+      })
+      .catch((error) => {
+        console.error(
+          "There was an error removing the item from the cart",
+          error
+        );
+      });
+  };
+
+  const handleEmptyCart = () => {
+    commerce.cart
+      .empty()
+      .then((resp) => {
+        setCart(resp.cart);
+      })
+      .catch((error) => {
+        console.error("There was an error emptying the cart", error);
+      });
+  };
+
   return (
     <Router>
       <Mui theme={themeMui}>
         <ThemeProvider theme={theme}>
           <div className="App">
+            <Box
+              padding={1}
+              marginTop={1.5}
+              sx={{
+                position: "fixed",
+                right: 100,
+                justifyContent: "end",
+                display: "flex",
+              }}
+            >
+              <IconButton
+                aria-label="shopping-cart"
+                onClick={() => toggleForm(true)}
+                size="large"
+              >
+                <ShoppingCartIcon color="primary" fontSize="inherit" />
+              </IconButton>
+            </Box>
+            <CartDrawer
+              cart={cart}
+              isOpen={isOpen}
+              onClose={() => toggleForm(false)}
+              onUpdateCartQty={handleUpdateCartQty}
+              onRemoveFromCart={handleRemoveFromCart}
+              onEmptyCart={handleEmptyCart}
+            />
             <BurgerMenu />
             <GlobalStyle />
             <Switch>
-              <Route path="/boutique" component={Boutique} />
+              <Route
+                path="/boutique"
+                exact
+                render={(props) => {
+                  return (
+                    <Boutique
+                      {...props}
+                      products={products}
+                      onAddToCart={handleAddToCart}
+                    />
+                  );
+                }}
+              />
+              <Route
+                path="/checkout"
+                exact
+                render={(props) => {
+                  return (
+                    <Checkout {...props} cart={cart} products={products} />
+                  );
+                }}
+              />
               <Route path="/ma-démarche" component={MyApproach} />
               <Route path="/ensemble" component={CreateTogether} />
               <Route path="/personal-pieces" component={PersonalPiece} />
--- src/pages/Boutique.jsx
+++ src/pages/Boutique.jsx
@@ -1,130 +1,21 @@
-import { Box, Button, Grid, IconButton } from "@mui/material";
-import { useState } from "react";
-import { useCallback } from "react";
-import { useEffect } from "react";
-import { commerce } from "../lib/commerce";
-import CartDrawer from "../components/cart/CartDrawer";
-import Fab from "@mui/material/Fab";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Box, Button, Grid } from "@mui/material";
 
-const Boutique = () => {
-  const [isOpen, toggleForm] = useState(false);
-  const [cart, setCart] = useState({});
-  const [products, setProducts] = useState([]);
-
-  const fetchProducts = useCallback(() => {
-    commerce.products
-      .list()
-      .then((products) => setProducts(products.data))
-      .catch((error) => {
-        console.log("There was an error fetching the products", error);
-      });
-  }, []);
-
-  const fetchCart = useCallback(() => {
-    commerce.cart
-      .retrieve()
-      .then((cart) => {
-        setCart(cart);
-      })
-      .catch((error) => {
-        console.error("There was an error fetching the cart", error);
-      });
-  }, []);
-
-  useEffect(() => {
-    fetchCart();
-    fetchProducts();
-  }, [fetchCart, fetchProducts]);
-
-  const handleAddToCart = (productId, quantity) => {
-    commerce.cart
-      .add(productId, quantity)
-      .then((item) => {
-        setCart(item.cart);
-      })
-      .catch((error) => {
-        console.error("There was an error adding the item to the cart", error);
-      });
-  };
-
-  const handleUpdateCartQty = (lineItemId, quantity) => {
-    commerce.cart
-      .update(lineItemId, { quantity })
-      .then((resp) => {
-        setCart(resp.cart);
-      })
-      .catch((error) => {
-        console.log("There was an error updating the cart items", error);
-      });
-  };
-
-  const handleRemoveFromCart = (lineItemId) => {
-    commerce.cart
-      .remove(lineItemId)
-      .then((resp) => {
-        setCart(resp.cart);
-      })
-      .catch((error) => {
-        console.error(
-          "There was an error removing the item from the cart",
-          error
-        );
-      });
-  };
-
-  const handleEmptyCart = () => {
-    commerce.cart
-      .empty()
-      .then((resp) => {
-        setCart(resp.cart);
-      })
-      .catch((error) => {
-        console.error("There was an error emptying the cart", error);
-      });
-  };
-
+const Boutique = (props) => {
   return (
     <>
-      <Box
-        padding={1}
-        marginTop={1.5}
-        sx={{
-          position: "fixed",
-          right: 100,
-          justifyContent: "end",
-          display: "flex",
-        }}
-      >
-        <IconButton
-          aria-label="shopping-cart"
-          onClick={() => toggleForm(true)}
-          size="large"
-        >
-          <ShoppingCartIcon color="primary" fontSize="inherit" />
-        </IconButton>
-      </Box>
       <Grid container>
-        {products.map((product) => (
+        {props.products.map((product) => (
           <Box padding={4}>
             <Grid item xs={3}>
               <p>{product.name}</p>
               <img src={product.image.url} alt="yop" width="300" />
-              <Button onClick={() => handleAddToCart(product.id, 1)}>
+              <Button onClick={() => props.onAddToCart(product.id, 1)}>
                 Add to cart
               </Button>
             </Grid>
           </Box>
         ))}
       </Grid>
-      <CartDrawer
-        cart={cart}
-        isOpen={isOpen}
-        onClose={() => toggleForm(false)}
-        onUpdateCartQty={handleUpdateCartQty}
-        onRemoveFromCart={handleRemoveFromCart}
-        onEmptyCart={handleEmptyCart}
-      />
     </>
   );
 };
