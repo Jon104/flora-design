7fa942f4ae5687c9855395ccee02ce6e915e24a0
Jonathan Girard <jonathan.girard@vooban.com>
2022-01-31 23:03:37 -0500

    Now filtering out a specific product out of the store

--- src/services/ProductServices.js
+++ src/services/ProductServices.js
@@ -1,16 +1,21 @@
 export const groupProductsByCategory = (products) =>
-  products.reduce((acc, d) => {
-    if (d.categories.length < 1)
-      acc.push({ id: "NoID", name: "No Category", data: [d] });
-    else {
-      const found = acc.find((a) => a.id === d.categories[0].id);
-      if (!found)
-        acc.push({
-          id: d.categories[0].id,
-          name: d.categories[0].name,
-          data: [d],
-        });
-      else found.data.push(d);
-    }
-    return acc;
-  }, []);
+  products
+    .filter((product) => {
+      if (product.id === "prod_nPEVlNQY3A5a7d") return false;
+      else return true;
+    })
+    .reduce((acc, d) => {
+      if (d.categories.length < 1)
+        acc.push({ id: "NoID", name: "Autres", data: [d] });
+      else {
+        const found = acc.find((a) => a.id === d.categories[0].id);
+        if (!found)
+          acc.push({
+            id: d.categories[0].id,
+            name: d.categories[0].name,
+            data: [d],
+          });
+        else found.data.push(d);
+      }
+      return acc;
+    }, []);
