1c48e316f5cc95df02edca9244fce7907f1ac224
Jonathan Girard <jonathan.girard@vooban.com>
2022-03-22 22:06:51 -0400

    Limited slides increments and decrements

--- src/components/common/ImageSlider/index.js
+++ src/components/common/ImageSlider/index.js
@@ -3,16 +3,25 @@ import Slides from "./Slides";
 import PropTypes from "prop-types";
 
 const ImageSlider = (props) => {
+  const onLeftClick = () => {
+    if (props.slideIndex < -1) props.setSlideIndex(props.slideIndex + 1);
+  };
+
+  const onRightClick = () => {
+    if (props.slideIndex > -Math.abs(props.slides.length))
+      props.setSlideIndex(props.slideIndex - 1);
+  };
+
   return (
     <Slides>
-      <button onClick={() => props.setSlideIndex(props.slideIndex + 1)} />
+      <button onClick={onLeftClick} />
 
       {props.slides.map((slide, i) => {
         let offset = props.slides.length + (props.slideIndex - i);
         return <Slide slide={slide} offset={offset} key={i} />;
       })}
 
-      <button onClick={() => props.setSlideIndex(props.slideIndex - 1)} />
+      <button onClick={onRightClick} />
     </Slides>
   );
 };
