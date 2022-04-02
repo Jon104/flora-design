dc715ba34196d0a147586f945392aae5b152746c
Jonathan Girard <jonathan.girard@vooban.com>
2022-04-02 07:58:21 -0400

    Added backdrop on loading animation

--- src/boutique/pages/Boutique.jsx
+++ src/boutique/pages/Boutique.jsx
@@ -1,4 +1,4 @@
-import { Box, CircularProgress, Grid } from "@mui/material";
+import { Backdrop, Box, CircularProgress, Grid } from "@mui/material";
 import { isMobile } from "react-device-detect";
 import { Subtitle } from "services/TypoService";
 import Product from "../components/Product";
@@ -10,17 +10,22 @@ const Boutique = ({ onAddToCart, productsByCategory }) => {
     if (cartIsLoaded()) return;
 
     return (
-      <Grid container xs={12} justifyContent="center">
-        <Box pt={8}>
-          <CircularProgress />
-        </Box>
-      </Grid>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={true}
+      >
+        <Grid container xs={12} justifyContent="center">
+          <Box pt={8}>
+            <CircularProgress />
+          </Box>
+        </Grid>
+      </Backdrop>
     );
   };
 
   return (
     <>
-      <Box padding={isMobile ? 1 : 8} paddingTop={18}>
+      <Box padding={isMobile ? 1 : 8} paddingTop={26}>
         <Grid container xs={12} alignItems="end">
           {renderLoading()}
           {productsByCategory.map((category) => (
