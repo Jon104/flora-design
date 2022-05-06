0523867bbebc911fd7c6c08dd7636b1cd4e697ad
Jonathan Girard <jonathan.girard@vooban.com>
2022-05-05 23:04:54 -0400

    Adjusted cart styling for gotocheckout

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -3,9 +3,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import CartItem from "./CartItem";
 import CloseIcon from "@mui/icons-material/Close";
+import styled from "styled-components";
+import { secondaryColor } from "../../styles";
+import { useHistory } from "react-router-dom";
 
 const Cart = (props) => {
+  const history = useHistory();
   const isCartEmpty = () => props.cart.total_unique_items === 0;
+  const { cart } = props;
 
   const renderEmptyCart = () => {
     if (!isCartEmpty()) return;
@@ -21,8 +26,6 @@ const Cart = (props) => {
   };
 
   const renderCart = () => {
-    const { cart } = props;
-
     if (isCartEmpty()) return;
 
     return (
@@ -36,28 +39,13 @@ const Cart = (props) => {
               onRemoveFromCart={props.onRemoveFromCart}
             />
           ))}
-        <Grid
-          sx={{
-            position: "sticky",
-            backgroundColor: "white",
-            bottom: 0,
-            paddingBottom: "2rem",
-          }}
-          container
-          justifyContent="flex-end"
-        >
-          <Grid item xs={8} />
-          <Grid item xs={2}></Grid>
-
-          <Grid item xs={3}>
-            <p>Total: {cart.subtotal && cart.subtotal.formatted_with_symbol}</p>
-            <Link to="/checkout" onClick={props.onClose}>
-              Passer la commande
-            </Link>
-          </Grid>
-        </Grid>
       </>
     );
+  };
+
+  const handleGoToCheckout = () => {
+    props.onClose();
+    history.push("/checkout");
   };
 
   return (
@@ -76,15 +64,55 @@ const Cart = (props) => {
         </Grid>
       </Box>
 
-      <Box px={2}>
+      <Box px={2} pb={16}>
         <Box pb={4}>
           <h4>Votre panier</h4>
         </Box>
         {renderEmptyCart()}
         {renderCart()}
       </Box>
+
+      <TotalAmount>
+        <Box pr={8}>
+          <Grid container justifyContent="end">
+            <Grid item>
+              <b>
+                <p>
+                  Total: {cart.subtotal && cart.subtotal.formatted_with_symbol}
+                </p>
+              </b>
+            </Grid>
+          </Grid>
+        </Box>
+      </TotalAmount>
+
+      <GoToCheckout onClick={handleGoToCheckout}>
+        <Grid container justifyContent="center">
+          <Grid item>
+            <h3>Passer la commande</h3>
+          </Grid>
+        </Grid>
+      </GoToCheckout>
     </Drawer>
   );
 };
+
+const GoToCheckout = styled.div`
+  position: fixed;
+  background-color: ${secondaryColor};
+  color: white;
+  bottom: 0;
+  width: 100%;
+  height: 4rem;
+`;
+
+const TotalAmount = styled.div`
+  position: fixed;
+  background-color: white;
+  padding-bottom: 4rem;
+  bottom: 0;
+  width: 100%;
+  box-shadow: 0px 20px 16px 14px #00000054;
+`;
 
 export default Cart;
---
+++ src/styles.js
@@ -0,0 +1,1 @@
+export const secondaryColor = "#9F2E0E";
