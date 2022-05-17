d29dca3f70e2f0351467516e08b69e5b72bd94c8
Jonathan Girard <jonathan.girard@vooban.com>
2022-05-17 00:07:57 -0400

    Added stuff in cache

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,7 +5,19 @@ precacheAndRoute(self.__WB_MANIFEST);
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open("mysite-static-v3").then(function (cache) {
-      return cache.addAll(["/vid/main_video.mp4"]);
+      return cache.addAll([
+        "/vid/main_video.mp4",
+        "/vid/vertical.mp4",
+        "/img/logo.png",
+        "/img/main_alt.png",
+        "/img/middle_section_long_image.jpeg",
+        "/img/my_approach.png",
+        "/img/panaches_main.png",
+        "/img/panaches_main_mobile.png",
+        "/img/personalPiece_mobile.png",
+        "/img/personalPiece.png",
+        "/img/landing_middle_view.jpg",
+      ]);
     })
   );
 });
