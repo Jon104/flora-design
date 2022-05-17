1ef65b44e180423a67676bf52c0cc22c1179a064
Jonathan Girard <jonathan.girard@vooban.com>
2022-05-17 00:29:17 -0400

    Re-added filename in cache paths

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,8 +4,20 @@ precacheAndRoute(self.__WB_MANIFEST);
 
 self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open("flora-design").then(function (cache) {
-      return cache.addAll(["/vid", "/img"]);
+    caches.open("flora-design-v1").then(function (cache) {
+      return cache.addAll([
+        "/vid/main_video.mp4",
+        "/vid/vertical.mp4",
+        "/img/logo.png",
+        "/img/main_alt.png",
+        "/img/middle_section_long_image.jpeg",
+        "/img/my_approach.png",
+        "/img/panaches_main.png",
+        "/img/panaches_main_mobile.png",
+        "/img/personalPiece_mobile.png",
+        "/img/personalPiece.png",
+        "/img/landing_middle_view.jpg",
+      ]);
     })
   );
 });
