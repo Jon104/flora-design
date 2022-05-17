30c125ffda2b6a758cdb1b60e2c8c265e3970f47
Jonathan Girard <jonathan.girard@vooban.com>
2022-05-16 23:57:22 -0400

    Fixing manifest error

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,3 +1,7 @@
+import { precacheAndRoute } from "workbox-precaching";
+
+precacheAndRoute(self.__WB_MANIFEST);
+
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open("mysite-static-v3").then(function (cache) {
