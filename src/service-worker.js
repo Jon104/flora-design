33868583c418b4403e087cec77e1ebe397b308bb
Jonathan Girard <jonathan.girard@vooban.com>
2022-05-17 00:21:53 -0400

    Another test

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,7 +5,7 @@ precacheAndRoute(self.__WB_MANIFEST);
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open("flora-design").then(function (cache) {
-      return cache.addAll(["./vid", "./img"]);
+      return cache.addAll(["/vid", "/img"]);
     })
   );
 });
